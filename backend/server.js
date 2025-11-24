1b9110bf8e27d15c787b534e39c2ee016bb46ddd
Malka Anjum <malkaanjum1999@gmail.com>
2025-11-24 23:24:01 +0530

    Backend Set Done

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const sequelize = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("MySQL connected"))
+  .catch((err) => console.log("DB Error: ", err));
+
+sequelize.sync().then(() => {
+  console.log("Database synced");
+});
+
