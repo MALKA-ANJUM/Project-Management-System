afbef1f0926457268b73d4455abd1a98830147e5
Malka Anjum <malkaanjum1999@gmail.com>
2025-12-10 23:45:00 +0530

    navbar Ui added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+	const { user, logout } = useContext(AuthContext);
+	return (
+		<nav className='navbar navbar-dark bg-dark px-3'>
+			<span className="navbar-brand">Project Manager</span>
+			<div className="text-white">
+				{user?.name}
+				<button className='btn btn-sm btn-outline-light ms-3' onClick={logout}>
+					Logout
+				</button>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
