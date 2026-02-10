da26446fe0de855a883503c609d20f06dde971f2
Malka Anjum <malkaanjum1999@gmail.com>
2026-02-10 23:21:48 +0530

    server setup in backend done

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const sequelize = require("./config/db");
+import express from "express";
+import cors from "cors";
+import mysql from "mysql";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import cookieParser from "cookie-parser";
+
 const app = express();
-
-/* ✅ LOAD MODELS (IMPORTANT) */
-require("./models/User");
-require("./models/Project");
-require("./models/ProjectMembers");
-require("./models/Task");
-require("./models/TaskComment");
-require("./models/ActivityLog");
-
-/* Routes */
-const authRoutes = require("./routes/authRoutes");
-const projectRoutes = require("./routes/projectRoutes");
-const taskRoutes = require("./routes/taskRoutes");
-const taskCommentRoutes = require("./routes/taskCommentRoutes");
-const activityLogRoutes = require("./routes/activityLogRoutes");
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
-/* API Routes */
-app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/tasks", taskRoutes);
-app.use("/api/comments", taskCommentRoutes);
-app.use("/api/activity-logs", activityLogRoutes);
-
-/* DB Connection */
-sequelize
-  .authenticate()
-  .then(() => console.log("MySQL connected"))
-  .catch((err) => console.log("DB Error:", err));
-
-sequelize.sync().then(() => {
-  console.log("Database synced");
-});
-
-/* Start Server */
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
